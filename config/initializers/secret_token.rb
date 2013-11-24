583c91e052dd1be1e42636a207bcf6dc28feb25b
Nikkihj <dogs@dogsoutandabout.com.au>
2013-11-24 12:43:35 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nhjdog::Application.config.secret_key_base = '368033c3bfa474e410d280fd47591c897e05c79cfcefd737ec3fc591fe448c5785e9cd34ca7cf4052231bb8498985a7deb32b3a3d24ba8c7a2f11a30507337c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nhjdog::Application.config.session_store :cookie_store, key: '_nhjdog_session'
